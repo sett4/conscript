fa79ef0cf5a32967746ed1fbc1597cacecddd6ee
FJT Sett4 <sett4.112@gmail.com>
2011-11-30 17:27:44 +0900

    launchconfig url modified

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ CS=$HOME/.conscript
 CSCS=$CS/n8han/conscript/cs
 CLC=$CSCS/launchconfig
 mkdir -p $CSCS
-curl https://raw.github.com/n8han/conscript/master/src/main/conscript/cs/launchconfig \
+curl https://raw.github.com/sett4/conscript/setup-modified/src/main/conscript/cs/launchconfig \
     > $CLC
 echo "
 [boot]
